5a3975ad0b008b8296db54297cb2309ae344d469
greg-mich <gregmich95@gmail.com>
2020-06-16 01:49:23 -0500

    removed modal form and made into normal form due to complexity of form. Added nested form arrays for excercies and sets. Still need to work on formatting and finishing form group for a set, as well as validations

--- src/app/lifting/add-resistance-training-session/add-resistance-training-session.component.ts
+++ src/app/lifting/add-resistance-training-session/add-resistance-training-session.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormArray, FormControl } from '@angular/forms';
 import { DatePipe } from "@angular/common";
+import { MessageBannerService } from "../../MessageBanner/messageBannerService";
+import { BannerMessage, BannerMessageType } from 'src/app/MessageBanner/messageBanner.model';
 
 @Component({
   selector: 'add-resistance-training-session',
@@ -10,34 +12,67 @@ import { DatePipe } from "@angular/common";
 export class AddResistanceTrainingSessionComponent implements OnInit {
 
   trainingSessionForm: FormGroup;
+  showForm: boolean = false;
 
-  constructor() { }
+  constructor(private messageBannerService: MessageBannerService) { }
 
   ngOnInit(): void {
-    // transforming current date to set to default to today
-    let transformedDate = 
-      new DatePipe(navigator.language)
-       .transform(new Date(), 'y-MM-dd');
-    this.trainingSessionForm = new FormGroup({
-      date: new FormControl(transformedDate, []),
-      excercises: new FormArray([])
-    });
+    this.initForm();
   }
 
   addExcercise() {
     console.log('Adding new excercise form group')
     this.excercises.push(new FormGroup({
-      excerciseName: new FormControl('')
+      excerciseName: new FormControl(''),
+      sets: new FormArray([])
     }));
+  }
+
+  addSet(index) {
+    const excercisesFormArray = this.trainingSessionForm.get("excercises") as FormArray;
+    const controls = excercisesFormArray.controls[index];
+    const sets = controls.get('sets') as FormArray;
+    sets.push(new FormGroup({
+      testName: new FormControl('hello world')
+    }))
+    console.log(sets);
   }
 
   get excercises() {
     return this.trainingSessionForm.get("excercises") as FormArray;
   }
 
+  getSets(index) {
+    console.log('getting sets');
+    const excercisesFormArray = this.trainingSessionForm.get("excercises") as FormArray;
+    const controls = excercisesFormArray.controls[index];
+    const sets = controls.get('sets') as FormArray;
+    console.log(sets);
+    console.log(sets.controls);
+    return sets;
+  }
+
   removeExcercise(index: number)
   {
     this.excercises.removeAt(index);
+  }
+
+  cancelAddExcercise() {
+    this.initForm();
+    this.messageBannerService.reportMessage(
+      new BannerMessage("Cancelled creating new resistance training session", BannerMessageType.info));
+  }
+
+  private initForm() {
+    // transforming current date to set to default to today
+    let transformedDate = 
+    new DatePipe(navigator.language)
+     .transform(new Date(), 'y-MM-dd');
+    this.showForm = false;
+    this.trainingSessionForm = new FormGroup({
+      date: new FormControl(transformedDate, []),
+      excercises: new FormArray([])
+    });
   }
 
 }
